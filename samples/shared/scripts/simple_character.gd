2b8999f673801b6f46ab46fd8e7700bb062aed6e
Jon Manning <jon@secretlab.com.au>
2026-04-25 12:02:25 +1000

    WIP 'welcome' sample

--- samples/shared/scripts/simple_character.gd
+++ samples/shared/scripts/simple_character.gd
@@ -3,9 +3,8 @@ extends CharacterBody3D
 @export var speed = 1
 
 func _input(_event: InputEvent) -> void:
-	var input = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	velocity = Vector3(input.x, 0, input.y) * speed;
-
+	# var input = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	# velocity = Vector3(input.x, 0, input.y) * speed;
 	pass
 
 # Called when the node enters the scene tree for the first time.
